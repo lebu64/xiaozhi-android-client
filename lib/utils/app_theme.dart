3a38013b311285af0759782dc793bd4225bb3163
User <gack@myway.de>
2025-10-04 14:24:09 +0200

    adjustments

--- lib/utils/app_theme.dart
+++ lib/utils/app_theme.dart
@@ -70,10 +70,12 @@ class AppTheme {
       type: BottomNavigationBarType.fixed,
       elevation: 8,
     ),
-    cardTheme: CardTheme(
+    cardTheme: const CardThemeData(
       color: Colors.white,
-      elevation: 1,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+      elevation: 1.0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(8)),
+      ),
     ),
     inputDecorationTheme: InputDecorationTheme(
       filled: true,
